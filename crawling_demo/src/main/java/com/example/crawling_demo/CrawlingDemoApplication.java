d7c6fc34f51897f6ed5dd0fda99bb27097dc5a20
bigstar017 <335005@naver.com>
2023-06-14 08:45:48 +0900

    make project

---
+++ crawling_demo/src/main/java/com/example/crawling_demo/CrawlingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.crawling_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlingDemoApplication.class, args);
+	}
+
+}
---
+++ crawling_demo/src/test/java/com/example/crawling_demo/CrawlingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crawling_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
